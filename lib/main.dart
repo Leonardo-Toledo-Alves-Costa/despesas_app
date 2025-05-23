1961f83125fc7ee784680c10f7e646ea67269fee
Leonardo Toledo <d2025010842@unifei.edu.br>
2025-05-23 17:37:43 -0300

    Mudanças na data das transações com o pacote INTL

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:despesas_app/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(DespesasApp());
 
@@ -62,14 +63,14 @@ class HomePage extends StatelessWidget {
                         margin: EdgeInsets.all(15),
                         decoration: BoxDecoration(border: Border(right: BorderSide(color: Colors.black, width: 1))),
                         padding: EdgeInsets.all(10),
-                        child: Text('R\$${tr.price.toString()}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                        child: Text('R\$${tr.price.toStringAsFixed(2)}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
                       ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(child: Text(tr.title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
                         ),
-                        Container(child: Text(tr.date.toString(), style: TextStyle(fontSize: 15, color: Colors.grey[700]),)
+                        Container(child: Text(DateFormat('d MMM y').format(tr.date), style: TextStyle(fontSize: 15, color: Colors.grey[700]),)
                         )
                       ],
                   )
