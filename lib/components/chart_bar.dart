67e7782d7547d43361f32f26b88418d118ba8f4d
Leonardo Toledo <d2025010842@unifei.edu.br>
2025-05-25 22:19:14 -0300

    Criação da parte de porcentagem do gráfico

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -18,10 +18,33 @@ class ChartBar extends StatelessWidget {
         Container(
           height: 50,
           width: 10,
-          child: Stack(),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        SizedBox(height: 5,),
-        Text(label)
+        const SizedBox(height: 5),
+        Text(label!),
       ],
     );
   }
